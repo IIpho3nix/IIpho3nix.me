24e547b8644b480fa34627a9536f1ad2e1e3b2ff
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2022-04-03 19:04:33 +0300

    redesing website

--- js/index.js
+++ js/index.js
@@ -2,7 +2,3 @@ if (window.location.search !== "") {
   window.location.href =
     "https://github.com/IIpho3nix/" + window.location.search.substring(1);
 }
-
-function redirect(path) {
-  window.location.href = path;
-}
