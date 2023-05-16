06c1fbed5cf76cc52d0ba73661bdb299a764f599
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2023-05-16 22:10:35 +0300

    auto focus

--- js/home-after.js
+++ js/home-after.js
@@ -4,6 +4,10 @@ if (savedSearchEngine) {
     searchEngineSelect.value = savedSearchEngine;
 }
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('search-input').focus();
+});
+
 searchEngineSelect.addEventListener('change', () => {
     var selectedSearchEngine = searchEngineSelect.value;
     localStorage.setItem('searchEngine', selectedSearchEngine);
