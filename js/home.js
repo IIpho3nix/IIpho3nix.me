5909bd1a2c56d1a8e0a2759e39aea3b4bfe77591
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2023-05-17 22:17:46 +0300

    added youtube to search bar added blur and turned if else to switch

--- js/home.js
+++ js/home.js
@@ -1,26 +1,43 @@
 function search() {
-    var query = document.getElementById("search-input").value;
-    var selectedEngine = document.getElementById("search-engine").value;
-    
-    var searchUrl;
-    if (selectedEngine === "google") {
-        searchUrl = "https://www.google.com/search?q=" + encodeURIComponent(query);
-    } else if (selectedEngine === "bing") {
-        searchUrl = "https://www.bing.com/search?q=" + encodeURIComponent(query);
-    } else if (selectedEngine === "duckduckgo") {
-        searchUrl = "https://duckduckgo.com/?q=" + encodeURIComponent(query);
-    } else if (selectedEngine === "startpage") {
-        searchUrl = "https://www.startpage.com/do/search?query=" + encodeURIComponent(query);
-    } else if (selectedEngine === "you") {
-        searchUrl = "https://you.com/search?q=" + encodeURIComponent(query);
-    }
-    
-    window.open(searchUrl, "_top");
+  var query = document.getElementById("search-input").value;
+  var selectedEngine = document.getElementById("search-engine").value;
+
+  var searchUrl;
+  switch (selectedEngine) {
+    case "google":
+      searchUrl =
+        "https://www.google.com/search?q=" + encodeURIComponent(query);
+      break;
+    case "bing":
+      searchUrl = "https://www.bing.com/search?q=" + encodeURIComponent(query);
+      break;
+    case "duckduckgo":
+      searchUrl = "https://duckduckgo.com/?q=" + encodeURIComponent(query);
+      break;
+    case "startpage":
+      searchUrl =
+        "https://www.startpage.com/do/search?query=" +
+        encodeURIComponent(query);
+      break;
+    case "you":
+      searchUrl = "https://you.com/search?q=" + encodeURIComponent(query);
+      break;
+    case "youtube":
+      searchUrl =
+        "https://www.youtube.com/results?search_query=" +
+        encodeURIComponent(query);
+      break;
+    default:
+      searchUrl = "";
+      break;
+  }
+
+  window.open(searchUrl, "_top");
 }
 
-document.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        search();
-    }
+document.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    search();
+  }
 });
