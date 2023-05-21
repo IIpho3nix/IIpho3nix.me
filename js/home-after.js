d93b31c9f7bf660d5317c9410e90060be2ce13f8
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2023-05-21 17:15:04 +0300

    reformating

--- js/home-after.js
+++ js/home-after.js
@@ -1,14 +1,14 @@
-var searchEngineSelect = document.getElementById('search-engine');
-var savedSearchEngine = localStorage.getItem('searchEngine');
+var searchEngineSelect = document.getElementById("search-engine");
+var savedSearchEngine = localStorage.getItem("searchEngine");
 if (savedSearchEngine) {
-    searchEngineSelect.value = savedSearchEngine;
+  searchEngineSelect.value = savedSearchEngine;
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('search-input').focus();
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("search-input").focus();
 });
 
-searchEngineSelect.addEventListener('change', () => {
-    var selectedSearchEngine = searchEngineSelect.value;
-    localStorage.setItem('searchEngine', selectedSearchEngine);
+searchEngineSelect.addEventListener("change", () => {
+  var selectedSearchEngine = searchEngineSelect.value;
+  localStorage.setItem("searchEngine", selectedSearchEngine);
 });
--- js/projects-fetcher.js
+++ js/projects-fetcher.js
@@ -1,23 +1,35 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.github.com/users/IIpho3nix/repos', true);
-xhr.responseType = 'json';
+xhr.open("GET", "https://api.github.com/users/IIpho3nix/repos", true);
+xhr.responseType = "json";
 xhr.onload = () => {
-    for (let i = 0; i < xhr.response.length; i++) {
-        document.body.innerHTML += `<div class="horizontal"><a class="project" href="${xhr.response[i].html_url}" rel="noreferrer" target="_top" >
+  for (let i = 0; i < xhr.response.length; i++) {
+    document.body.innerHTML += `<div class="horizontal"><a class="project" href="${
+      xhr.response[i].html_url
+    }" rel="noreferrer" target="_top" >
         <br>
         <h2>${xhr.response[i].name}</h2>
         <br>
-        <p>${xhr.response[i].description == null ? "No Description Found" : xhr.response[i].description}</p>
+        <p>${
+          xhr.response[i].description == null
+            ? "No Description Found"
+            : xhr.response[i].description
+        }</p>
         <br>
         <p>${xhr.response[i].stargazers_count} stars</p>
         <br>
         <p>${xhr.response[i].watchers_count} watchers</p>
         <br>
-        <p> License: ${xhr.response[i].license == null ? "Not Found" : xhr.response[i].license.name}</p>
+        <p> License: ${
+          xhr.response[i].license == null
+            ? "Not Found"
+            : xhr.response[i].license.name
+        }</p>
         <br>
-        <p> Last updated: ${new Date(Date.parse(xhr.response[i].updated_at)).toDateString()}</p>
+        <p> Last updated: ${new Date(
+          Date.parse(xhr.response[i].updated_at)
+        ).toDateString()}</p>
         <br>
         </a></div><br>`;
-    }
+  }
 };
 xhr.send();
