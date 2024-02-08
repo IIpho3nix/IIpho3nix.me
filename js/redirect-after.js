62acbf5022ac5d822465e1854929f12c8898bbb3
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2024-02-08 20:54:58 +0300

    oops its supossed to be https

--- js/redirect-after.js
+++ js/redirect-after.js
@@ -1,7 +1,7 @@
 if (window.location.search !== "") {
   if (window.location.search.substring(1).startsWith("intrl=")) {
     document.getElementById("frame").src =
-      "http://" +
+      "https://" +
       location.host +
       "/html/" +
       window.location.search.substring(7) +
