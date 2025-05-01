3cd7c11212ab1978663dccd787a0201586ef1e85
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2025-05-01 18:04:03 +0300

    a bit of polish for the projects page

--- js/projects-fetcher.js
+++ js/projects-fetcher.js
@@ -12,23 +12,23 @@ xhr.onload = () => {
         <br>
         <h2>${xhr.response[i].name}</h2>
         <br>
-        <p>${
+        <p style="color: #dedede;">${
           xhr.response[i].description == null
             ? "No Description Found"
             : xhr.response[i].description
         }</p>
         <br>
-        <p>${xhr.response[i].stargazers_count} stars</p>
+        <p style="color: #dedede;">${xhr.response[i].stargazers_count} stars</p>
         <br>
-        <p>${xhr.response[i].watchers_count} watchers</p>
+        <p style="color: #dedede;">${xhr.response[i].watchers_count} watchers</p>
         <br>
-        <p> License: ${
+        <p style="color: #dedede;"> License: ${
           xhr.response[i].license == null
             ? "Not Found"
             : xhr.response[i].license.name
         }</p>
         <br>
-        <p> Last updated: ${new Date(
+        <p style="color: #dedede;"> Last updated: ${new Date(
           Date.parse(xhr.response[i].updated_at)
         ).toDateString()}</p>
         <br>
